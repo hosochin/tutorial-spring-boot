360a5e2aec9b31528829b3438fa6d79313431d10
hosochin <sisimaimainomai@gmail.com>
2020-07-23 10:51:12 +0900

    自作アノテーション実装

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.myannotation.MyAnnotation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("/sample")
+@RequestMapping
 public class SampleController {
 
-    @GetMapping
-    public String get() {
-        return "Hello World";
+    /**
+     * リクエストパラメータの文字列を大文字に変換して返すcontroller
+     */
+    @GetMapping("/upper-case")
+//    @MyAnnotation(conversionType = MyAnnotation.ConversionType.UPPER)
+    public String getUpperCase(@RequestParam(name = "message") String message) {
+        ArrayList<String> list = new ArrayList();
+        ArrayList list2 = new ArrayList<>();
+        list2.add(1);
+        list = list2;
+        list.get(0);
+        return message;
+    }
+
+    /**
+     * リクエストパラメータの文字列を小文字に変換して返すcontroller
+     */
+    @GetMapping("/lower-case")
+    @MyAnnotation(conversionType = MyAnnotation.ConversionType.LOWER)
+    public String getLowerCase(@RequestParam(name = "message") String message) {
+        return message;
     }
 }

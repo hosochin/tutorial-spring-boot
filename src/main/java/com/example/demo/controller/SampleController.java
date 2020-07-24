f11b37279bf3946c88053f353375770cc22afdbe
hosochin <sisimaimainomai@gmail.com>
2020-07-25 08:28:07 +0900

    caffeineでキャッシュしてみた

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.SampleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sample")
+@RequiredArgsConstructor
+@Slf4j
 public class SampleController {
 
+    private final SampleService sampleService;
+
     @GetMapping
-    public String get() {
-        return "Hello World";
+    public String get(@RequestParam(name = "message") String message) {
+        log.info("処理開始");
+        String str = sampleService.sampleCache(message);
+        log.info("処理終了");
+        return str;
     }
 }
---
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SampleService {
+
+    @Cacheable("sampleCache")
+    public String sampleCache(String message) {
+        log.info("キャッシュ効いてない, message=" + message);
+        return message;
+    }
+}

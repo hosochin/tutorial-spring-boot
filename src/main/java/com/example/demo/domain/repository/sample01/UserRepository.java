2b9af0e93c6feff61877fbbd95413afad9b570dc
hosochin <sisimaimainomai@gmail.com>
2020-08-17 22:28:23 +0900

    select省略実装完了

--- src/main/java/com/example/demo/domain/repository/sample01/UserRepository.java
+++ src/main/java/com/example/demo/domain/repository/sample01/UserRepository.java
@@ -1,9 +1,8 @@
-package com.example.demo.domain.repository.sample02;
+package com.example.demo.domain.repository.sample01;
 
 import com.example.demo.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository02 extends JpaRepository<UserEntity, String> {
-}
+public interface UserRepository extends JpaRepository<UserEntity, String> {}
+
--- src/main/java/com/example/demo/domain/repository/sample01/UserRepository01.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.repository.sample01;
-
-import com.example.demo.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository01 extends JpaRepository<UserEntity, String> {
-}

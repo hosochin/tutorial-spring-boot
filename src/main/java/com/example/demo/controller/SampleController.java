bcc8a3042db351046d8aab7d8ea5ff5014966bfd
hosochin <sisimaimainomai@gmail.com>
2021-03-05 21:21:47 +0900

    例題２実装

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sample")
 public class SampleController {
 
-    @GetMapping
-    public String get() {
+    @GetMapping("/sample")
+    public String get1() {
         return "Hello World";
+    }
+
+    @GetMapping("/sample2")
+    public String get2(String keyword) {
+        return keyword;
     }
 }

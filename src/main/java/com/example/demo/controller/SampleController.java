4b2c4d71c45d403345b517813fe3a45da989dd33
hosochin <sisimaimainomai@gmail.com>
2023-04-05 10:39:54 +0900

    webクライアント対応

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,44 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.resource.Message;
+import com.example.demo.service.TutorialRestTemplate;
+import com.example.demo.service.TutorialWebClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/sample")
+@RequiredArgsConstructor
 public class SampleController {
 
-    @GetMapping
-    public String get() {
-        return "Hello World";
+    private final TutorialRestTemplate tutorialRestTemplate;
+    private final TutorialWebClient tutorialWebClient;
+
+    // RestTemplateのGET
+    @GetMapping("/restTemplate")
+    public Message getRestTemplate() {
+        return tutorialRestTemplate.get();
+    }
+
+    // RestTemplateのPOST
+    @PostMapping("/restTemplate")
+    public void postRestTemplate(@RequestBody Message message) {
+        tutorialRestTemplate.post(message);
+    }
+
+    // WebClientのGET
+    @GetMapping("/webClient")
+    public Message getWebClient() {
+        return tutorialWebClient.get();
+    }
+
+    // WebClientのPOST
+    @PostMapping("/webClient")
+    public void postWebClient(@RequestBody Message message) {
+        tutorialWebClient.post(message);
     }
 }

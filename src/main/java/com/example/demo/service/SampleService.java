4e507611eb0673d36ed740aa9348437f9672224a
hosochin <sisimaimainomai@gmail.com>
2025-12-27 20:43:55 +0900

    環境変数で制御

--- src/main/java/com/example/demo/service/SampleService.java
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -11,8 +11,11 @@ public class SampleService {
   private final FeatureFlags featureFlags;
 
   public String execute() {
+    // DIして使う場合
     //    if (featureFlags.newUiEnabled()) return "機能フラグON";
     //    return "機能フラグOFF";
+
+    // static参照
     if (FeatureFlags.getInstance().newUiEnabled()) return "機能フラグON";
     return "機能フラグOFF";
   }
